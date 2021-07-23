5f9824da27ba50b60f8c7e4901444f6e0a3c7943
戚肇烽 <861388974@qq.com>
2021-07-23 14:28:26 +0800

    完成了登录页面

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,7 @@
 <template>
-<div>Home组件</div>
+<div>
+    <el-button type="info" @click="logout">退出</el-button>
+</div>
 </template>
 <script>
 export default {
@@ -10,7 +12,12 @@ export default {
         };
     },
     methods:{
-        
+        logout(){
+            //清空token
+            window.sessionStorage.clear()
+            //跳转到登录页面
+            this.$router.push('/login')
+        }
     }
   
 }
